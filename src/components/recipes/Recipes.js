f0189a66dd4f5fa421a27c8cc44c177830e9a48b
Blake Fletcher <blkfltchr@gmail.com>
2018-12-23 10:37:27 -0500

    Recipe filtering by meal works on single click

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -8,13 +8,66 @@ import RecipeCard from './RecipeCard';
 
 class Recipes extends Component {
 
+  state = {
+    recipes: []
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.recipes !== prevProps.recipes) {
+      this.setState({
+        recipes: this.props.recipes
+      })
+    }
+  }
+
+  filterAllRecipes = () => {
+    let allRecipes = this.state.recipes
+    allRecipes = allRecipes.filter((recipe) => {
+      return recipe.meal !== ""
+    })
+    this.setState({
+      recipes: allRecipes
+    })
+    console.log(this.state.recipes)
+  }
+
+  filterBreakfastRecipes = () => {
+    let breakfastRecipes = this.state.recipes
+    breakfastRecipes = breakfastRecipes.filter((recipe) => {
+      return recipe.meal === "breakfast"
+    })
+    this.setState({
+      recipes: breakfastRecipes
+    })
+    console.log(this.state.recipes)
+  }
+
+  filterLunchRecipes = () => {
+    let lunchRecipes = this.state.recipes
+    lunchRecipes = lunchRecipes.filter((recipe) => {
+      return recipe.meal === "lunch"
+    })
+    this.setState({
+      recipes: lunchRecipes
+    })
+    console.log(this.state.recipes)
+  }
+
+  filterDinnerRecipes = () => {
+    let dinnerRecipes = this.state.recipes
+    dinnerRecipes = dinnerRecipes.filter((recipe) => {
+      return recipe.meal === "dinner"
+    })
+    this.setState({
+      recipes: dinnerRecipes
+    })
+    console.log(this.state.recipes)
+  }
+
   render() {
     const { recipes } = this.props;
-
-    // const lunchRecipes = this.props.recipes.filter(recipe => {
-    //   return recipe.meal === "lunch";
-    // }) ; 
-
+    console.log("Recipe state", this.state.recipes)
+    console.log("Recipe props", recipes)
     if (recipes) {
       return (
         <div className="row">
@@ -27,10 +80,14 @@ class Recipes extends Component {
                     <i className="fas fa-utensils" /> Recipes{' '}
                   </h2>
                 </div>
+                <button onClick={this.filterBreakfastRecipes}>All</button>
+                <button onClick={this.filterBreakfastRecipes}>Breakfast</button>
+                <button onClick={this.filterLunchRecipes}>Lunch</button>
+                <button onClick={this.filterDinnerRecipes}>Dinner</button>
               </div>
               <div className="row">
                 <div style={{display: "flex", flexWrap: "wrap", width: "85vw", margin: "0 auto", justifyContent: "center"}}>
-                  {recipes.map(recipe => (
+                  {this.state.recipes.map(recipe => (
                     <RecipeCard id={recipe.id} recipe={recipe} />  
                   ))}
                   <AddRecipeCard />
