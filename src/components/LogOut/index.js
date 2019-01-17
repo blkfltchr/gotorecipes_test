c53676530f0105961af53489f585e97b8b6606f8
Blake Fletcher <blkfltchr@gmail.com>
2019-01-17 17:38:48 -0500

    Conditional auth is not janky, doSignOut was mis-typed

--- src/components/LogOut/index.js
+++ src/components/LogOut/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { withFirebase } from '../Firebase';
 
 const LogOutButton = ({ firebase }) => (
-    <button type="button" className="btn btn-danger" onClick={firebase.doSignout}>
+    <button type="button" className="btn btn-danger" onClick={firebase.doSignOut}>
         <i className="fa fa-sign-out"></i>
     </button>
 );
